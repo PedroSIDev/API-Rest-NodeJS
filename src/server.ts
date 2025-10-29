deb442fa2f248e547f67964b5a279e9eca1bcc12
Pedro <pedhenriquecm@gmail.com>
2025-10-29 14:31:58 -0300

    feat/fix: add new DB functions and env | fix validations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transaction
 })
 
-app.listen({ port: 3333 }).then(() => {
-  console.log('Server is running on http://localhost:3333')
-})
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server is running http://localhost/' + env.PORT)
+  })
